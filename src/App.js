6a4dd8fecbab4fc6e20c313a6cee49983b36662c
rahmanasad222 <asadur.rehman@cheetay.pk>
2023-06-24 18:05:16 +0500

    responses with gpt 3.5

--- src/App.js
+++ src/App.js
@@ -53,13 +53,13 @@ console.log('user input', userInput.prompt)
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    // const defaultSystemContent = `You are an AI language model trained to assist recruiters in refining job posts and your name is recruiterGPT. do not generate a response if the job description and some requirements are not given, and ask for them. It assists users in generating human-like text based on the given instructions and context. think properly and take your time before answering. Here are the instructions: Assistant, please generate a ${userInput.style.toLowerCase()} and vibrant job description for the position. The goal is to rewrite the existing job description, emphasizing the benefits and opportunities associated with the role. Take on the persona of ${userInput.persona}, a recruitment expert, and create the content in a ${userInput.style.toLowerCase()}  style that will attract potential candidates. Present the information in a compelling manner while keeping the user's requirements in mind. Even if certain points are not present in the job description, mention them and create enthusiasm around them. These Points include: 1- Offer a Competitive Compensation and Benefits.
-    //       2- Vibrant and collaborative team,
-    //       3- Professional Development Opportunities.
-    //       4- Work-Life Balance.
-    //       5- Offering Challenging and Meaningful Work.
-    //       6- Become part of our family. 7- Career Development Plan. Prioritize communicating what's in it for them. Emphasize more on benefits for them and highlight the benefits and gains they can expect from the job.Also write about the essential requirements and qualifications needed in detail. First emphasize on tonality and benefits and then generate refined requirements. Remember, do not generate response if no actual job description is given to you, if it is not there, ask for it. Thank you!.`;
-const defaultSystemContent = `you are an assistant to human. Generate human-like text. follow the instructions to generate refined job posts just like ${userInput.persona}. follow the persona of ${userInput.persona} in a ${userInput.style.toLowerCase()} tone. emphasize more on what benefits the candidate will get and make them vibrant and detailed. Rewrite requirements and what the candidate has to do in the job. rewrite the job description and create enthusiasm around it. length should be more than 800 words. remember always emphasize on what's in it for the candidates first in detail, remember assistant, in detail. Inform the candidates more about the benefits and engage them. Then give the requirements.`
+const defaultSystemContent = `you are an intelligent system to assist the user in rewriting a job description in human-like text.
+System, Take on the persona of ${userInput.persona} to attract potential candidates.
+System you must use a ${userInput.style.toLowerCase()} tonality and engage the reader.
+System, you have to be ${userInput.style.toLowerCase()} the reader with what's in it for them using five or more points and these should engage the reader.
+System, also refine the responsibilities and requirements and describe them using proper points with explanation. you can use upto 2000 words.
+System, use your own creativity to yield even better results then following these instructions.
+System, if a job description is not given, ask for it.`
     setUserInput(prevState => ({
       ...prevState,
       system: defaultSystemContent
@@ -67,7 +67,6 @@ const defaultSystemContent = `you are an assistant to human. Generate human-like
   }, [userInput.persona, userInput.style]);
 
   useEffect(() => {
-    // const defaultUserContent =  `Take the persona of ${userInput.persona} and use a ${userInput.style.toLowerCase()} tonality when rewriting the following Job Description. In the job description emphasize what’s in it for them.First include Career Development, training and growth opportunities, work-life balance, competitive salary, challenging and meaningful work and a vibrant and collaborative team. More of the content should be around these points infact 68% of you response should be around benefits and what an employee can get from us. emphasize less on the requirements, but explain them after describing benefits. Display response in a Job Description format and include a few of the main responsibilities. Generate content no more than 3500 words, But more than 1000 words. Note: If no information about the job is given in this prompt, ask for it. do not generate response if no job description and title is given to you.`;
 const defaultUserContent = ''
           setUserInput(prevState => ({
             ...prevState,
